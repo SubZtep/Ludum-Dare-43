68c7b7183b07b1da3be90965efc9851046440772
Andras Serfozo <subztep@gmail.com>
2018-12-02 07:00:04 +0000

    :wheelchair: Debug options and Webpack build mode

--- src/js/objects/environment.js
+++ src/js/objects/environment.js
@@ -1,4 +1,5 @@
 import { scene } from "Engine/engine"
+import 'babylonjs-inspector'
 
 export let ground = null
 
@@ -36,16 +37,6 @@ function createGround () {
 
 function createObtacles () {
 
-  /* var _box = BABYLON.MeshBuilder.CreateBox("myBox", {height: 1, width: 2, depth: 0.5}, scene)
-  _box.rotation.y = -0.9
-  _box.position.z = -3
-  _box.physicsImpostor = new BABYLON.PhysicsImpostor(_box, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene)
-
-  var _box2 = BABYLON.MeshBuilder.CreateBox("myBox", {height: 1, width: 2, depth: 0.5}, scene)
-  _box2.rotation.y = 0.7
-  _box2.position.z = 0
-  _box2.physicsImpostor = new BABYLON.PhysicsImpostor(_box2, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene) */
-
   let box1 = BABYLON.MeshBuilder.CreateBox("box1", { height: 1, width: 2, depth: 0.5 }, scene)
   box1.rotation.y = Math.PI / 5
   box1.position = new BABYLON.Vector3(-1, 0, 5)
@@ -55,4 +46,33 @@ function createObtacles () {
   box2.rotation.y = -Math.PI / 5
   box2.position = new BABYLON.Vector3(1.5, 0, 6)
   box2.physicsImpostor = new BABYLON.PhysicsImpostor(box2, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene)
+
+  if (!PRODUCTION) {
+    createGizmoManager([box1, box2])
+  }
+}
+
+
+function createGizmoManager (meshes) {
+
+  scene.debugLayer.show()
+
+  let gizmoManager = new BABYLON.GizmoManager(scene)
+  gizmoManager.boundingBoxGizmoEnabled = true
+  gizmoManager.attachableMeshes = meshes
+
+  document.onkeydown =  e => {
+    if (e.key == 'w') {
+        gizmoManager.positionGizmoEnabled = !gizmoManager.positionGizmoEnabled
+    }
+    if (e.key == 'e') {
+        gizmoManager.rotationGizmoEnabled = !gizmoManager.rotationGizmoEnabled
+    }
+    if (e.key == 'r') {
+        gizmoManager.scaleGizmoEnabled = !gizmoManager.scaleGizmoEnabled
+    }
+    if (e.key == 'q') {
+        gizmoManager.boundingBoxGizmoEnabled = !gizmoManager.boundingBoxGizmoEnabled
+    }
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,60 @@
 const
   path = require('path'),
-  HtmlWebpackPlugin = require('html-webpack-plugin')
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  webpack = require('webpack')
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/js/main.js'),
-  resolve: {
-    enforceExtension: false,
-    alias: {
-      Engine: path.resolve(__dirname, 'src/js/engine/'),
-      Scene: path.resolve(__dirname, 'src/js/scene/'),
-      Objects: path.resolve(__dirname, 'src/js/objects/'),
-      Classes: path.resolve(__dirname, 'src/js/classes/')
-    }
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        use: 'pug-loader'
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+module.exports = function(env, argv) {
+  let conf = {
+    entry: {
+      main: path.resolve(__dirname, 'src/js/main.js'),
+      vendor: ['babylonjs', 'oimo', 'cannon']
+    },
+
+    resolve: {
+      enforceExtension: false,
+      alias: {
+        Engine: path.resolve(__dirname, 'src/js/engine/'),
+        Scene: path.resolve(__dirname, 'src/js/scene/'),
+        Objects: path.resolve(__dirname, 'src/js/objects/'),
+        Classes: path.resolve(__dirname, 'src/js/classes/')
       }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/pug/index.pug')
-    })
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.pug$/,
+          use: 'pug-loader'
+        },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        }
+      ]
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src/pug/index.pug')
+      }),
+      new webpack.DefinePlugin({
+        PRODUCTION: argv.mode === 'production'
+      })
+    ],
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js'
+      //'[name]-[hash].js' (check for separated vendor hash)
+    },
+
+    optimization: {
+      splitChunks: {
+        // include all types of chunks
+        chunks: 'all'
+      }
+    }
   }
+
+  return conf
 }
